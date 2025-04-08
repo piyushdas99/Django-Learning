fe3006f6adfceedb770960863516e293c7e2593c
piyushdas99 <piyushmanaswi123@gmail.com>
2025-04-09 00:42:26 +0530

    added templates and understood its interaction with views file

--- playground/views.py
+++ playground/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse("Hello World")
+    # return HttpResponse("Hello World")
+    return render(request,'hello.html')
     
 
 # Create your views here.
